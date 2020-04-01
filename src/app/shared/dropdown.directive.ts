2db55392913693eae40cbd1222cf6f73835532b3
szeszej <szeszej@gmail.com>
2020-04-01 08:28:28 +0200

    enhanced dropdown directive

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -8,17 +8,12 @@ export class DropdownDirective {
   constructor(private element: ElementRef, private renderer: Renderer2) {
   }
 
-  @HostListener("click") openDrowndown(event: Event){
-      let part = this.element.nativeElement.querySelector('.dropdown-menu');
-    if (part.classList.contains("show")) {
-
+  @HostListener("document:click", ['$event']) openDrowndown(event: Event) {
+    let part = this.element.nativeElement.querySelector('.dropdown-menu');
+    if (part.classList.contains("show") || (this.element.nativeElement.contains(event.target) === false)) {
       this.renderer.removeClass(part, "show")
     } else {
       this.renderer.addClass(part, "show")
     }
-
   }
-
-
-
 }
