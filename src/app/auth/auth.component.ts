7f45c167b3948b86b98d2701a8f0075b597191c7
szeszej <szeszej@gmail.com>
2020-05-16 22:43:12 +0200

    registering and logging in now works

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms";
-import { AuthService } from "./auth.service";
+import { AuthService, AuthResponseData } from "./auth.service";
 import { User } from "./user.model";
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-auth',
@@ -26,19 +27,25 @@ export class AuthComponent implements OnInit {
     if (!form.valid) {
       return;
     }
+    let user = new User(form.value.email, form.value.password)
     this.isLoading = true;
     this.error = null;
+    let authObservable: Observable<AuthResponseData>
     if (this.loginMode) {
-
+      authObservable = this.authService.login(user)
     } else {
-      this.authService.register(new User(form.value.email, form.value.password)).subscribe(response => {
-        console.log(response);
-        this.isLoading = false;
-      }, errorMessage => {
-        this.error = errorMessage;
-        this.isLoading = false;
-      })
+      authObservable = this.authService.register(user)
     }
+
+    authObservable.subscribe(response => {
+      console.log(response);
+      this.isLoading = false;
+    }, errorMessage => {
+      console.log(errorMessage);
+      this.error = errorMessage;
+      this.isLoading = false;
+    })
+
     form.reset()
   }
 
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { User } from "./user.model";
 import { tap, catchError } from "rxjs/operators";
 import { throwError } from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   expiresIn: string;
   localId: string;
   refreshToken: string;
+  registered?: boolean;
 }
 
 @Injectable({
@@ -23,20 +24,29 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   register(user: User) {
-    return this.http.post<AuthResponseData>("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBVKlL7MMJauWwA4sxffFukWFVp7tTXkwM", user).pipe(catchError(
-      error => {
-        let errorMessage: string = "Nieznany błąd";
-        if (!error.error || !error.error.error ) {
-          return throwError(errorMessage)
-        }
-        switch (error.error.error.message) {
-          case "EMAIL_EXISTS": errorMessage = "Konto o takim emailu już istnieje";
-            break;
-          default: errorMessage = error.message;
-            break;
-        }
-        return throwError(errorMessage)
-      }
-    ), tap((response: AuthResponseData) => this.token = response.idToken))
+    return this.http.post<AuthResponseData>("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBVKlL7MMJauWwA4sxffFukWFVp7tTXkwM", user).pipe(catchError(this.handleError))
+    // , tap((response: AuthResponseData) => this.token = response.idToken)
+  }
+
+  login(user: User) {
+    return this.http.post<AuthResponseData>("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBVKlL7MMJauWwA4sxffFukWFVp7tTXkwM", user).pipe(catchError(this.handleError))
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage: string = "Nieznany błąd";
+    if (!error.error || !error.error.error ) {
+      return throwError(errorMessage)
+    }
+    switch (error.error.error.message) {
+      case "EMAIL_EXISTS": errorMessage = "Konto o takim emailu już istnieje";
+        break;
+      case "INVALID_PASSWORD": errorMessage = "Nieprawidłowe hasło";
+        break;
+      case "EMAIL_NOT_FOUND": errorMessage = "Nieprawidłowy email";
+        break;
+      default: errorMessage = error.message;
+        break;
+    }
+    return throwError(errorMessage)
   }
 }
